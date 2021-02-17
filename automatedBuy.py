8d5d91cad966bdc05f7c4fca4089458e82aaa939
Elias Landsman <elandsma@unca.edu>
2021-02-17 18:23:45 -0500

    pseudocode for automatedbuy

--- automatedBuy.py
+++ automatedBuy.py
@@ -3,6 +3,8 @@
 pseudocode
     receive asset to buy (ETH/BTC/Other)
     receive amount in USD to purchase
+    Buys have $10 minimum so make sure amount is >$10 (or perhaps write logic in to batch until reaches $10 minimum)
     receive frequency to make purchase
-    create bot
-    make the buys
+    create bot  (inherit bot from botclass?)
+    make the buys on set schedule
+    Record information about transaction into a JSON - either just the transaction ID, or more info (entire return statement) - TBD
