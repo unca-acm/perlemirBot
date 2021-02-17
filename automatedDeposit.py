ca8e872cba2a7979d39c8cd9c4cc60a74e7a227e
Elias Landsman <elandsma@unca.edu>
2021-02-17 18:20:28 -0500

    automateddepositchanges

--- automatedDeposit.py
+++ automatedDeposit.py
@@ -5,6 +5,7 @@ import json
 import botclass
 import sys
 import schedule
+from botclass import bot
 
 #USAGE: automatedDeposit.py USDamount Frequency
 
@@ -15,7 +16,7 @@ import schedule
 #    make it work
 
 #trying to use inheritance here
-class automatedDeposit(botclass):
+class automatedDeposit(bot):
     def __init__(self, frequency, amount):
         apiKey = cfg.api['API_KEY']
         apiSecret = cfg.api['API_SECRET']
@@ -23,18 +24,14 @@ class automatedDeposit(botclass):
         sandbox = cfg.api['SANDBOX']
         super().init(apiKey, apiSecret, apiPassphrase, sandbox)
         self.frequency = frequency
-        self.amount=amount
+        self.amount = amount
         #TODO: add check here to make sure amount is > 10
+        #also add a check to make sure API config file is correct
+        #perhaps here is also a good place to query API and make sure the account is active and has privileges.
 
 
-#get API info from config file
-apiKey = cfg.api['API_KEY']
-apiSecret = cfg.api['API_SECRET']
-apiPassphrase =cfg.api['API_PASSPHRASE']
-sandbox = cfg.api['SANDBOX']
 
-#make a new bot object
-myBot = botclass.bot(apiKey, apiSecret, apiPassphrase, sandbox)
+myBot = automatedDeposit(10, 10)
 print(myBot)
 
 #get primary payment method ID
