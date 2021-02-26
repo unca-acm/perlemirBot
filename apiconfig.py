095186a8343e622e16437308cce71bc24f9de918
Elias <60270361+elandsma@users.noreply.github.com>
2021-02-26 17:20:29 -0500

    Update apiconfig.py
    capitalized 'T'. Python is case sensitive for booleans

--- apiconfig.py
+++ apiconfig.py
@@ -2,7 +2,7 @@ api = {
     "API_KEY": 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     "API_SECRET": 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx==',
     "API_PASSPHRASE": 'xxxxxxxxxx',
-    "SANDBOX": 'true',
+    "SANDBOX": 'True',
 }
 #Sandbox is a boolean flag for debug mode. If true, the bot will connect to the sandbox api. If false, it will connect to real API.
 #The API credentials are different. Your real key won't work in the sandbox, your sandbox key won't in the live API.
