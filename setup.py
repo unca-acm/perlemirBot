1a7aa2a60ce4cd74b0ce21febc15345705817585
Ethan Green <egreen4@unca.edu>
2021-03-09 14:38:49 -0500

    removed url and deps from setup.py, hopefully this works

--- setup.py
+++ setup.py
@@ -2,7 +2,5 @@ from distutils.core import setup
 
 setup(name='perlemirBot',
       version='1.0',
-      description='Perlemir bot pipenv package test',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
+      description='Perlemir bot pipenv package test'
      )
